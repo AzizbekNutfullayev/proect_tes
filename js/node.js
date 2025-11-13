e6a14b119174f96ae711fc5f5160373ea0746784
Nutfullayev <nutfullayevazizbek5@gmail.com>
2025-11-13 18:26:23 +0500

    comit

--- js/node.js
+++ js/node.js
@@ -1,21 +1,5 @@
 const links = document.querySelectorAll('.course_link a');
 const cards = document.querySelectorAll('.course_card .card');
-document.getElementById('nextPage').addEventListener('click', function() {
-  window.location.href = "./about.html";
-});
-
-document.getElementById('goToLogin').addEventListener('click', function() {
-  window.location.href = "./page/login.html";
-})
-document.getElementById('goTo').addEventListener('click', function() {
-  window.location.href = "./page/login.html";
-})
-document.getElementById('nextP').addEventListener('click', function() {
-  window.location.href = "./page/about.html";
-})
-document.getElementById('nextPa').addEventListener('click', function() {
-  window.location.href = "./page/about.html";
-})
 
 links.forEach(link => {
   link.addEventListener('click', e => {
@@ -41,3 +25,19 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
+document.getElementById('nextPage').addEventListener('click', function() {
+  window.location.href = "./about.html";
+});
+
+document.getElementById('goToLogin').addEventListener('click', function() {
+  window.location.href = "./page/login.html";
+})
+document.getElementById('goTo').addEventListener('click', function() {
+  window.location.href = "./page/login.html";
+})
+document.getElementById('nextP').addEventListener('click', function() {
+  window.location.href = "./page/about.html";
+})
+document.getElementById('nextPa').addEventListener('click', function() {
+  window.location.href = "./page/about.html";
+})
